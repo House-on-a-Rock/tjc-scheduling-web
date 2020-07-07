bb68380685b80a2de7856300a0d7bcc057dbcd26
Shaun Tung <stung@datacolor.com>
2020-07-07 14:38:40 -0400

    some odd prettier changes

--- src/store/reducers/authReducer.ts
+++ src/store/reducers/authReducer.ts
@@ -1,26 +1,26 @@
 import { AuthActionTypes, LOGIN, LOGOUT, AuthState } from '../types';
 
 const initialState: AuthState = {
-    isLoggedIn: true,
-    isValidLogin: null,
+  isLoggedIn: true,
+  isValidLogin: null,
 };
 
 export const authReducer = (state = initialState, action: AuthActionTypes): AuthState => {
-    switch (action.type) {
-        case LOGIN:
-            return {
-                ...state,
-                isLoggedIn: true,
-                isValidLogin: true,
-            };
-        case LOGOUT:
-            return {
-                ...state,
-                isLoggedIn: false,
-                isValidLogin: null,
-            };
+  switch (action.type) {
+    case LOGIN:
+      return {
+        ...state,
+        isLoggedIn: true,
+        isValidLogin: true,
+      };
+    case LOGOUT:
+      return {
+        ...state,
+        isLoggedIn: false,
+        isValidLogin: null,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
