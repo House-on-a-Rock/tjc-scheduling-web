43dc488376ee5ced05a7743332fa72c00d263bdb
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-07-10 22:06:57 -0400

    renaming

--- src/components/Main/Members.tsx
+++ src/components/Main/Members.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 // material UI
-import { makeStyles, withStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { fade, makeStyles, withStyles, Theme, createStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -17,16 +17,18 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 import CSS from 'csstype';
 
 // actions
 import { getAllUsers } from '../../store/apis';
 
 // dummy data
-import data from './MembersData';
+import data from './membersDatabase';
 
 // types
-import {userType} from '../../shared/types/membersData';
+import {UserType} from '../../shared/types/membersModel';
 
 let index = -1;
 
@@ -37,21 +39,21 @@ const styleHead: CSS.Properties = {
 export const Members = () => {
     const classes = useStyles();
     const rows = data;
-    const [selected, setSelected] = React.useState<string[]>([]);
+    const [selected, setSelected] = useState<string[]>([]);
     const isSelected = (name: string) => selected.indexOf(name) !== -1;
-    const [selectUser, setSelectUser] = React.useState<any>({
+    const [selectUser, setSelectUser] = useState<UserType>({
       firstName: '',
       lastName: '',
       email: '',
+      church: '',
       roles: []
     });
 
-    const handleClick = (event: React.MouseEvent<unknown>, row: userType) => {
+    const handleClick = (event: React.MouseEvent<unknown>, row: UserType) => {
       let newSelected: string = row.firstName;
       index = rows.findIndex(rows => rows.firstName === newSelected);
       setSelectUser(row);
       setSelected([newSelected]);
-      console.log(typeof(selectUser))
     }
     return (
       <Grid container spacing={3}>
@@ -95,7 +97,22 @@ export const Members = () => {
                 <Table className={classes.table} aria-label="simple table">
                     <TableHead>
                       <TableRow >
-                          <TableCell style={styleHead}>First&nbsp;Name</TableCell>
+                        <div className={classes.search}>
+                          <div className={classes.searchIcon}>
+                            <SearchIcon />
+                          </div>
+                          <InputBase
+                            placeholder="Search…"
+                            classes={{
+                              root: classes.inputRoot,
+                              input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                          />
+                        </div>
+                      </TableRow>
+                      <TableRow >
+                          <TableCell style={styleHead} align="left">First&nbsp;Name</TableCell>
                           <TableCell style={styleHead} align="left">Last&nbsp;Name</TableCell>
                           <TableCell style={styleHead} align="left">Email</TableCell>
                           <TableCell style={styleHead} align="left">Church</TableCell>
@@ -110,7 +127,6 @@ export const Members = () => {
                             hover
                             onClick={event => {
                               handleClick(event, row)
-                              // console.log(selected)
                             }}
                             selected={isitemSelected} 
                             key={row.firstName}
@@ -132,8 +148,47 @@ export const Members = () => {
     );
 };
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
     table: {
       minWidth: 650,
-    },
-});
+    }
+  })
+)
--- src/shared/types/membersModel.ts
+++ src/shared/types/membersModel.ts
@@ -1,4 +1,4 @@
-export interface userType {
+export interface UserType {
   firstName: string;
   lastName: string;
   email: string;
