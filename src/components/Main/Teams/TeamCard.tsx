b6e7ac54a7af5b4ee0e051ed8976d0375ee2b96d
Shaun Tung <stung@datacolor.com>
2020-07-26 20:12:03 -0400

    refactored and added delete and add member functions

--- src/components/Main/Teams/TeamCard.tsx
+++ src/components/Main/Teams/TeamCard.tsx
@@ -1,10 +1,4 @@
 import React from 'react';
-import {
-  Droppable,
-  Draggable,
-  DraggableProvided,
-  DroppableProvided,
-} from 'react-beautiful-dnd';
 import { MembersData, DraggedItem } from './models';
 
 // Material UI Components
@@ -12,13 +6,8 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Typography from '@material-ui/core/Typography';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
+import { DroppableTeamMembersList } from './DroppableTeamMemberList';
 
 interface TeamCardProps {
   role: string;
@@ -47,7 +36,7 @@ export const TeamCard = ({ role, members, draggedItem }: TeamCardProps) => {
       </CardContent>
       <Divider orientation="vertical" flexItem />
       <CardContent className={classes.list} style={{ overflow: 'auto' }}>
-        <DroppableMembers
+        <DroppableTeamMembersList
           role={role}
           canDrop={canDrop}
           members={members}
@@ -55,63 +44,6 @@ export const TeamCard = ({ role, members, draggedItem }: TeamCardProps) => {
         />
       </CardContent>
     </Card>
-  );
-};
-
-interface DroppableMembersProps {
-  role: string;
-  members: MembersData[];
-  canDrop: () => boolean;
-  draggedItem: DraggedItem;
-}
-
-const DroppableMembers = ({
-  role,
-  members,
-  canDrop,
-  draggedItem,
-}: DroppableMembersProps) => {
-  const classes = useStyles();
-  console.log(draggedItem);
-  return (
-    <Droppable droppableId={role} key={role} isDropDisabled={canDrop()}>
-      {(provided: DroppableProvided) => (
-        <List dense ref={provided.innerRef} className={classes.list}>
-          {members.map((member: MembersData, index: number) => {
-            return draggedItem.source === '' || !(draggedItem.source === 'USERBANK') ? (
-              <Draggable draggableId={member.id} index={index} key={member.id}>
-                {(provided: DraggableProvided) => (
-                  <ListItem
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    <ListItemText id={member.id} primary={member.name} />
-                    <ListItemSecondaryAction>
-                      <IconButton onClick={() => console.log(member, role, index)}>
-                        <DeleteIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                )}
-              </Draggable>
-            ) : (
-              <>
-                <ListItem>
-                  <ListItemText id={member.id} primary={member.name} />
-                  <ListItemSecondaryAction>
-                    <IconButton onClick={() => console.log(member, role, index)}>
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>
-              </>
-            );
-          })}
-          {provided.placeholder}
-        </List>
-      )}
-    </Droppable>
   );
 };
 
