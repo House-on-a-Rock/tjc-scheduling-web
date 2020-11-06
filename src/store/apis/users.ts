47319f5c2e7810f971a940bec6911f7683a4e652
Shaun Tung <stung@datacolor.com>
2020-11-05 19:33:35 -0500

    reimplemented authorization

--- src/store/apis/users.ts
+++ src/store/apis/users.ts
@@ -3,22 +3,32 @@ import { secretIp } from '../../../secrets/secretStuff';
 import { extractUserId } from '../../shared/utilities';
 import { AddUserProps } from '../../shared/types';
 
+const accessToken = localStorage.getItem('access_token');
+
 export function getAllUsers(): Promise<AxiosResponse> {
-  return axios.get(`${secretIp}/api/users`);
+  return axios.get(`${secretIp}/api/users`, {
+    headers: { authorization: accessToken },
+  });
 }
 
 export function getAllLocalMembers(churchId: number): Promise<AxiosResponse> {
-  return axios.get(`${secretIp}/api/users?churchId=${churchId}`);
+  return axios.get(`${secretIp}/api/users?churchId=${churchId}`, {
+    headers: { authorization: accessToken },
+  });
 }
 
 export function getUser(userId: number): Promise<AxiosResponse> {
-  return axios.get(`${secretIp}/api/users/${userId}`);
+  return axios.get(`${secretIp}/api/users/${userId}`, {
+    headers: { authorization: accessToken },
+  });
 }
 
 export function deleteUser(userId: number): Promise<AxiosResponse> {
-  const accessToken = localStorage.getItem('access_token');
   const loggedInUserId = extractUserId(accessToken);
-  if (loggedInUserId !== userId) return axios.delete(`${secretIp}/api/users/${userId}`);
+  if (loggedInUserId !== userId)
+    return axios.delete(`${secretIp}/api/users/${userId}`, {
+      headers: { authorization: accessToken },
+    });
 }
 
 export function addUser({
@@ -28,11 +38,17 @@ export function addUser({
   password,
   churchId,
 }: AddUserProps): Promise<AxiosResponse> {
-  return axios.post(`${secretIp}/api/users`, {
-    email: email,
-    firstName: firstName,
-    lastName: lastName,
-    password: password,
-    churchId: churchId,
-  });
+  return axios.post(
+    `${secretIp}/api/users`,
+    {
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      password: password,
+      churchId: churchId,
+    },
+    {
+      headers: { authorization: accessToken },
+    },
+  );
 }
